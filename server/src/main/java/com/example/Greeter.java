67f754d977bd10207d868fe11e7af628ec25e8b7
maiki93 <1153900+maiki93@users.noreply.github.com>
2021-07-02 15:05:09 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,13 +1,20 @@
 package com.example;
-
-
+/** Say Hello.
+ * @author Doug Lowe
+ * @author www.LoweWriter.com
+ * @version 1.5
+ * @since 1.0
+  */
 public class Greeter {
 
-  
+  /** Constructor */
   public Greeter() {
 
   }
 
+  /** add name
+  @param String someone
+  */
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
